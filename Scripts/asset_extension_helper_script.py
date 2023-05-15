a381f27fd08dee5ba7f33cbe1f518934f0c1979c
Somesh Karthik <87409736+someshkarthik-cred@users.noreply.github.com>
2023-05-15 09:08:25 +0530

    feat: moved extension related info to separate script

---
+++ Scripts/asset_extension_helper_script.py
@@ -0,0 +1,36 @@
+import sys
+import json
+
+function = sys.argv[1]
+
+with open('asset_extension_data.json', 'r') as f:
+    extension_data = json.load(f)
+
+if function == "extension_list":
+    extension_list = []
+    for item in extension_data["size_limit_table"]:
+        extension_str = ','.join(item["supported_extensions"] + item["unsupported_extensions"])
+        extension_list.append(extension_str)
+    print(",".join(extension_list))
+elif function == "size_limit":
+    size_limit_table = extension_data["size_limit_table"]
+    extension = sys.argv[2]
+    for item in size_limit_table:
+        supported_extenion=item["supported_extensions"]
+        if extension in supported_extenion:
+            print(item["limit"])
+            sys.exit(0)
+    sys.exit(1)
+elif function == "supported_extensions":
+    size_limit_table = extension_data["size_limit_table"]
+    print(",".join(",".join(item["supported_extensions"]) for item in size_limit_table))
+    sys.exit(0)
+elif function == "supported_extension_table":
+    markdown = "File type | Format | Threshold\n--- | --- | ---\n"
+    for item in extension_data["size_limit_table"]:
+        extensions = ', '.join([f"`{ext}`" for ext in item["supported_extensions"]])
+        markdown += f"{item['name']} | {extensions} | {item['limit']}KB\n"
+    print(markdown)
+else:
+    print("🚨 Unknown format. please fix the script for any discrepency 🚨")
+    sys.exit(1)
